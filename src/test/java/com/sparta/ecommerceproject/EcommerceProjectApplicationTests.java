61b774f488de3ec0de37eba066a4a422766344a7
tyler <tyler0702@naver.com>
2024-05-03 23:00:15 +0900

    Initial Commit

---
+++ src/test/java/com/sparta/ecommerceproject/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ecommerceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
