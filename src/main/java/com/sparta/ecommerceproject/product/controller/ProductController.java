8bffab490457b4885d196d1a80eb7a565911837d
tyler <tyler0702@naver.com>
2024-05-10 14:16:06 +0900

    :sparkles: 특정 api Secured annotation 적용.

--- src/main/java/com/sparta/ecommerceproject/product/controller/ProductController.java
+++ src/main/java/com/sparta/ecommerceproject/product/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.sparta.ecommerceproject.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,20 +18,21 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Secured("ROLE_ADMIN")
     @PostMapping
     public ResponseEntity<String> createAdminProduct(@RequestBody ProductRequestDto requestDto) {
         productService.createProduct(requestDto);
         return ResponseEntity.status(201)
                 .body("Product created successfully");
     }
-
+    @Secured("ROLE_ADMIN")
     @PutMapping("/{productId}")
     public ResponseEntity<String> updateProduct(@PathVariable Long productId, @RequestBody ProductUpdateRequest productRequest) {
         productService.updateProduct(productId, productRequest);
         return ResponseEntity.status(200)
                 .body("Product update successfully");
     }
-
+    @Secured("ROLE_ADMIN")
     @DeleteMapping("/{productId}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long productId){
         productService.deleteProduct(productId);
