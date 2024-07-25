c47e1da8096c42e3eee656be4b35c8cede4b8d22
tyler <tyler0702@naver.com>
2024-07-25 19:58:41 +0900

    :sparkles:Timestamp(Order Domain)

---
+++ src/main/java/com/sparta/ecommerceproject/global/Timestamped.java
@@ -0,0 +1,29 @@
+package com.sparta.ecommerceproject.global;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
--- src/main/java/com/sparta/ecommerceproject/user/controller/UserController.java
+++ src/main/java/com/sparta/ecommerceproject/user/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.sparta.ecommerceproject.user.controller;
 
 import com.sparta.ecommerceproject.global.jwt.JwtUtil;
+import com.sparta.ecommerceproject.global.security.UserDetailsImpl;
 import com.sparta.ecommerceproject.user.dto.LoginRequestDto;
 import com.sparta.ecommerceproject.user.dto.SignupRequestDto;
 import com.sparta.ecommerceproject.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import com.sparta.ecommerceproject.user.entity.User;
 import com.sparta.ecommerceproject.user.entity.UserRoleEnum;
@@ -37,6 +39,15 @@ public class UserController {
         jwtUtil.addJwtToCookie(token, response);
 
         return ResponseEntity.ok().body(loginedUser.getRole().toString());
+    }
+
+    @GetMapping("/loginstatus")
+    public ResponseEntity<Boolean> checkStatus(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        boolean status = true;
+        if(userDetails.getUser().getUsername().isBlank()){
+           status = false;
+        }
+        return ResponseEntity.ok().body(status);
     }
 }
 
