a2f35ed86b6cf2ccc1cbafae33af94e853a3d56d
tyler <tyler0702@naver.com>
2024-08-18 20:31:50 +0900

    :recycle:CI/CD 테스트

--- src/main/java/com/sparta/ecommerceproject/product/controller/ProductController.java
+++ src/main/java/com/sparta/ecommerceproject/product/controller/ProductController.java
@@ -52,6 +52,11 @@ public class ProductController {
         return ResponseEntity.status(200).body(productService.getAllProducts());
 
     }
+    @GetMapping("/testCICD")
+    public ResponseEntity<List<ProductResponse>> getAllProducts2(){
+        return ResponseEntity.status(200).body(productService.getAllProducts());
+
+    }
 
 
 }
