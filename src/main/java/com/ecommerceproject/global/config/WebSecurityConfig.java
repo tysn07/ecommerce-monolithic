869b0a3eee488183ec41a7b48f12b841ccb489f4
tysn07 <tyler0702@naver.com>
2024-08-29 21:14:38 +0900

    :recycle:permit health check

--- src/main/java/com/ecommerceproject/global/config/WebSecurityConfig.java
+++ src/main/java/com/ecommerceproject/global/config/WebSecurityConfig.java
@@ -54,14 +54,17 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                         .permitAll() // resources 접근 허용 설정
-                        .requestMatchers("**").permitAll() // '/member/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("**","/actuator/health").permitAll()
         );
 
         // 필터 관리
         http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
+
         return http.build();
 
     }
+
+
 
 }
