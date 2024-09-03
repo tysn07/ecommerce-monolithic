a593982db3f7142bc1e61b7ae5c042861e680fab
tysn07 <tyler0702@naver.com>
2024-09-03 16:23:49 +0900

    :recycle: edit Cors

--- src/main/java/com/ecommerceproject/global/config/CorsConfig.java
+++ src/main/java/com/ecommerceproject/global/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/products/all")
                 .allowedOrigins("https://son7shop.com","http://son7shop.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
