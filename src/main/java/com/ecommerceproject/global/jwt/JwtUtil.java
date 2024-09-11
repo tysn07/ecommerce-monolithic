9c721738151052604680cc26400427b88db23b6f
tysn07 <tyler0702@naver.com>
2024-09-11 17:46:27 +0900

    :wrench: fix merge error

--- src/main/java/com/ecommerceproject/global/jwt/JwtUtil.java
+++ src/main/java/com/ecommerceproject/global/jwt/JwtUtil.java
@@ -34,7 +34,7 @@ public class JwtUtil {
    // Base64 Encode 한 SecretKey
 
     @Value("$jwt_secret_key")
-    private String secretKey = "7Iqk7YyM66W07YOA7L2U65Sp7YG065+9U3ByaW5n6rCV7J2Y7Yqc7YSw7LWc7JuQ67mI7J6F64uI64ukLg==";
+    private String secretKey;
 
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
